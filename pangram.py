f44e53455ab4542682300dcc4149a3f9f1b7fdf1
Kaitlin Rosen <kayejrosen@gmail.com>
2022-06-12 16:48:41 -0700

    solved pangram without isalpha (learned of its existence today too)

--- pangram.py
+++ pangram.py
@@ -13,6 +13,16 @@ For example::
 def is_pangram(sentence):
     """Given a string, return True if it is a pangram, False otherwise."""
 
+    alpha = set(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+
+    sentence_set = set()
+
+    for char in sentence:
+        if char in alpha:
+            sentence_set.add(char)
+
+    return alpha == sentence_set
+
 
 if __name__ == "__main__":
     import doctest
